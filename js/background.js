0431c9f61206223d391a75ceed308935200c4f51
OlyoshaOlyosha <remaxux@proton.me>
2024-02-14 01:37:06 +0300

    Добавлены файлы расширения

---
+++ js/background.js
@@ -0,0 +1,31 @@
+chrome.action.onClicked.addListener((tab) => {
+    chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        function: `
+        let clickCount = 0;
+
+        // Если появилось окно с кнопкой, то нажать
+        setInterval(function() {
+            // Ищем модальное окно и кнопку внутри него
+            let modal = document.querySelector('.modal-dialog.modal-dialog-scrollable[data-region="modal"]');
+            let button = modal ? modal.querySelector('button.btn.btn-primary[data-action="save"]') : null;
+
+            // Нажать на кнопку
+            if (modal && button) {
+                button.click();
+                clickCount++;
+
+                // Удалить окно
+                let elementToRemove = document.querySelector('.modal-backdrop');
+                if (elementToRemove) {
+                    elementToRemove.remove();
+                }
+            }
+            // Программа пока не может нажимать больше 2 раз. Вместо этого перезагружается страница
+            if (clickCount >= 3) {
+                location.reload();
+            }
+        }, 500);
+        `
+    });
+});
